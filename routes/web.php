2291af9f1215db1e1b8b7a1e431b18bb6ecd7926
sirvalclear <ohukaiv@gmail.com>
2022-07-08 11:06:09 +0100

    modified auth route for email verification

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use App\Http\Middleware\Admin;
 use App\Http\Middleware\Staff;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\StaffController;
-
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ConfirmPasswordController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +25,31 @@ use App\Http\Controllers\StaffController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+  // Authentication Routes...
+  Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+  Route::post('login', [LoginController::class, 'login']);
+  Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+  
+  // Registration Routes...
+  Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+  Route::post('register', [RegisterController::class, 'register']);
+  
+  // Password Reset Routes...
+  Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+  Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+  Route::get('password/reset/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
+  Route::post('password/reset', [ForgotPasswordController::class, 'reset'])->name('password.update');
+  
+  // Confirm Password
+  Route::get('password/confirm', [ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+  Route::post('password/confirm', [ConfirmPasswordController::class, 'confirm']);
+  
+  // Email Verification Routes...
+  Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
+  Route::get('email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify'); 
+  Route::get('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+
 
 //admin middleware protected routes
 Route::middleware([Admin::class])->group(function()
@@ -40,6 +68,6 @@ Route::middleware([Staff::class])->group(function()
     Route::get('/staff/home', [StaffController::class, 'index'])->name('staff-home');
     Route::post('/staff/home', [StaffController::class, 'store'])->name('store');
 });
-Auth::routes();
+// Auth::routes(['verify' => true]);
 Route::get('/home', [HomeController::class, 'index']);
 
